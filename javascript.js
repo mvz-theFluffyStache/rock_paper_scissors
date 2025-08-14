3c0aaa90c9a33d4fda881544563903f5b418d939
mvz-theFluffyStache <120417863+mvz-theFluffyStache@users.noreply.github.com>
2025-08-14 13:52:16 +0200

    completed assignment

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
-    switch(computerChoice) {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    switch(choice) {
         case 1:
             return "Rock";
         case 2:
@@ -10,13 +10,60 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
-let humanChoice = prompt("Please enter Rock, Paper, or Scissors");
-
-function getPlayerChoice() {
-    return humanChoice = String(humanChoice).at(0).toUpperCase() + String(humanChoice).slice(1).toLowerCase();
+function getHumanChoice() {
+    let choice = prompt("Please enter Rock, Paper, or Scissors");
+    return choice = String(choice).at(0).toUpperCase() + String(choice).slice(1).toLowerCase();
 }
 
-console.log(getPlayerChoice());
+// Testing switch to make process simpler
+function winConditions(human, computer) {
+    const condition = `${human} ${computer}`;
+    switch(condition) {
+        case "Rock Scissors":
+        case "Paper Rock":
+        case "Scissors Paper":
+            return "Win";
+        case "Paper Scissors":
+        case "Scissors Rock":
+        case "Rock Paper":
+            return "Loss";
+        default:
+            return "Draw";
 
+    }
+}
+
+function playGame(rounds) {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        const outcome = winConditions(humanChoice, computerChoice);
+         if (outcome == "Win") {
+      console.log(`You win!`);
+      humanScore++;
+    } else if (outcome == "Loss") {
+      console.log(`You lose!`);
+      computerScore++;
+    } else {
+      console.log(`It's a tie! You both played ${humanChoice}`);
+    }
+
+    console.log(`Score is: ${humanScore} - ${computerScore}`);
+  }
+
+  for (let n = 0; n < rounds; n++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+
+  if (humanScore > computerScore)
+    console.log("Congratulations, you won the game!");
+  else if (computerScore > humanScore)
+    console.log("The computer won. Better luck next time!");
+  else
+    console.log("You and the computer are perfectly tied")
+}
+
+playGame(5);
